ccd475674f2656d9289b627a5a213b1e786e23f6
Gwuill <guill.branche@gmail.com>
2024-01-08 15:23:10 +0100

    rocket + box

---
+++ Assets/Scripts/Movement_rocket.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement_rocket : MonoBehaviour
+{
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Movement of the rocket with ZQSD and rotation of the rocket when left or right
+        float moveX = Input.GetAxis("Horizontal");
+        float moveY = Input.GetAxis("Vertical");
+        
+        Vector2 movement = new Vector2(moveX, moveY);
+        
+        movement.Normalize();
+        
+        Rigidbody2D rb = GetComponent<Rigidbody2D>();
+        rb.MovePosition(rb.position + movement * 10 * Time.deltaTime);
+        
+    }
+}
